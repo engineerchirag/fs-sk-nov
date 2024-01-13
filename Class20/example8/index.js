6847fdec9f9dfcb491a3d6734a786eac5c4e1a92
Chirag Goel <engineerchirag021@gmail.com>
2024-01-13 23:16:35 +0530

    Class20

---
+++ Class20/example8/index.js
@@ -0,0 +1,10 @@
+var bodyRef = document.querySelector('body');
+var spanRef = document.createElement('span');
+spanRef.innerText = 'Script1 Location';
+bodyRef.appendChild(spanRef);
+
+// for(var i = 0; i< 1000; i++) {
+//     console.log(i);
+// }
+
+// console.log(1);
