4e20c39a6acb37d0eedfda7c7bfb586c1b1a50b1
Chirag Goel <engineerchirag021@gmail.com>
2024-03-15 22:23:31 +0530

    Deployment demo

--- ReactJs/Class13/react-testing/src/App.js
+++ ReactJs/Class13/react-testing/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with Chirag
         </a>
         <Counter />
       </header>
